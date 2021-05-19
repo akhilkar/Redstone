a8f8f4f619bb370c27771658e0c4c49b4e08fe75
akhilkar <82723884+akhilkar@users.noreply.github.com>
2021-05-20 02:23:59 +0530

    Update settings.py

--- NotAgain/settings.py
+++ NotAgain/settings.py
@@ -122,6 +122,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'assets/')
 ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
